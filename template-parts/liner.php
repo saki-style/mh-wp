e67a1214ea6dd2add3d27e22c17d5442a513fea3
saki <73070512+sk-style@users.noreply.github.com>
2020-11-18 13:39:52 +0900

    Update liner.php

--- template-parts/liner.php
+++ template-parts/liner.php
@@ -6,7 +6,7 @@ $next_post = get_next_post();
 if( $prev_post ):?>
     <div class="linear-link _prev">
         <a href="<?php echo get_permalink( $prev_post->ID ); ?>">
-        <p class="linear-btn"><</p>
+        <p class="linear-btn">&lt;</p>
         <div class="linear-body">
             <time datetime="<?php echo get_the_time( 'c', $prev_post->ID ); ?>"><?php echo get_the_time( 'Y.n.j', $prev_post->ID ); ?></time>
             <p><?php echo get_the_title( $prev_post->ID ); ?></p>
@@ -21,7 +21,7 @@ if( $next_post ):?>
                 <time datetime="<?php echo get_the_time( 'c', $next_post->ID ); ?>"><?php echo get_the_time( 'Y.n.j', $next_post->ID ); ?></time>
                 <p><?php echo get_the_title( $next_post->ID ); ?></p>
             </div>
-        <p class="linear-btn">></p>
+        <p class="linear-btn">&gt;</p>
         </a>
     </div>
 <?php endif; ?>
